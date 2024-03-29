8aa74f66e47aea15ef4a1c795dca65b0d91fe051
Odil-Programming <odilchikfurqatovich@gmail.com>
2024-03-29 13:34:23 +0500

    for and while homework

---
+++ main.js
@@ -0,0 +1,42 @@
+let originalArray1 = [2, 4, 9, 10, 45, 67, 8, 90];
+let originalArray = [2, 4, 9, 10, 45, 67, 8, 90];
+let newArray = [];
+
+for (let i = 0; i < originalArray.length; i++) {
+    if (originalArray[i] % 2 === 0) {
+        newArray.push(originalArray[i]);
+    }
+}
+console.log(originalArray1);
+console.log(newArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
